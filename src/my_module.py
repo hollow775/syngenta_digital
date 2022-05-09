46ac56df48945176b602e906966aa6037cd9ea9e
Clayton R Prazeres <claytonimes@gmail.com>
2022-05-09 01:45:34 -0300

    Resolvendo o desafio

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,59 @@
-def get_cheapest_hotel(number):   #DO NOT change the function's name
-    cheapest_hotel = "cheapest_hotel_name"
+
+#Regular
+#Rewards
+def get_cheapest_hotel(number): 
+    lakewood_prices = [110, 90, 80, 80]
+    bridgewood_prices = [160, 60, 110, 50]
+    ridgewood_prices = [220, 150, 100, 40]
+    weekdays = ['mon', 'tues', 'wed', 'thu', 'fri']
+    weekends = ['sat', 'sun']
+    pay_weekdays = 0
+    pay_weekend = 0
+
+    client, date = number.split(": ") 
+    days_scheduled = date.split(", ")
+    if(client == 'Regular'):
+        start_position = 0
+    if(client == 'Rewards'):
+        start_position = 2
+
+    for day_scheduled in days_scheduled:
+        for day in weekdays:
+            if(day in day_scheduled):
+                pay_weekdays += 1
+        for day_end in weekends:
+            if(day_end in day_scheduled):
+                pay_weekend += 1 
+    
+    lakewood = lakewood_prices[start_position]*pay_weekdays + lakewood_prices[start_position+1] * pay_weekend
+    bridgewood = bridgewood_prices[start_position]*pay_weekdays + bridgewood_prices[start_position+1] * pay_weekend
+    ridgewood = ridgewood_prices[start_position]*pay_weekdays + ridgewood_prices[start_position+1]*pay_weekend
+
+    if(lakewood < bridgewood and lakewood < ridgewood):
+        cheapest_hotel = "Lakewood"
+    if(bridgewood < lakewood and bridgewood < ridgewood or lakewood == bridgewood):
+        cheapest_hotel = "Bridgewood"
+    if((ridgewood < lakewood and ridgewood < bridgewood) or lakewood == ridgewood or bridgewood == ridgewood):
+        cheapest_hotel = "Ridgewood"
+    #DO NOT change the function's name
     return cheapest_hotel
+
+#primeiramente - cortar a string
+#verificar valores
+#escolher 
+
+# Lakewood - 3 
+#           normal | fidelidade
+#Semana ->  110    |     80
+#Fim    ->  90     |     80
+
+
+#Bridgewood - 4 
+#           normal | fidelidade
+#Semana ->    160  |    110
+#Fim    ->    60   |    50
+
+#Ridgewood - 5
+#           normal | fidelidade
+#Semana ->    220  |    100
+#Fim    ->    150  |    40
